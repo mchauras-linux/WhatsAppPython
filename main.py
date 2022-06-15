f239b14f6040a967ad062d85ba0b806e9b04cffb
Mukesh Chaurasiya <mukeshmike9@gmail.com>
2022-06-15 20:44:19 +0530

    pyjokes added

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 import time
 import string
 import random
+import pyjokes
 
 driver = webdriver.Chrome('./chromedriverLinux')
  
@@ -16,7 +17,7 @@ wait = WebDriverWait(driver, 600)
  
 # Replace 'Friend's Name' with the name of your friend
 # or the name of a group
-target = '"Manisha"'
+target = '"Gudiya Singh"'
  
 x_arg = '//span[contains(@title,' + target + ')]'
 group_title = wait.until(EC.presence_of_element_located((
@@ -27,12 +28,13 @@ print("Searching InputBox")
 input_box = wait.until(EC.presence_of_element_located((
     By.XPATH, inp_xpath)))
 print("InputBox Found")
-for i in range(100):
+for i in range(1000):
     # Replace the below string with your own message
     message = str("Message ") + str(i) + str(": I wrote code for spamming Whatsapp. Gudiya Challenged me. Suffer my Wrath now :D")
 
     #Generate Random String
     #message = str(''.join(random.choices(string.ascii_uppercase + string.digits, k = 11)))
     print("Message number: " + str(i))
+    message = pyjokes.get_joke(category='all')
     input_box.send_keys( message + Keys.ENTER)
-    time.sleep(1)
+    time.sleep(60)
